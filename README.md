dc6f1d6933f6122eb8d5e54f538931f927580d73
Monaco <monaco@usp.br>
2020-05-09 20:34:07 -0300

    Directions

--- README.md
+++ README.md
@@ -1,3 +1,17 @@
-# Exercício: calcule a média aritimética
+# Introductory C programming exercise
 
-Exercício para cursto introdutório de programação.
+This is a companion programming exercise for an introductory course on C programming.
+
+## Directions
+
+ Complete the source code so that the program outputs the simple arithmetic average
+ of the values stored in variables v1, v2 and v3.
+
+ The program output shall comprise a real value with 6 decimal cases.
+
+ Please, do not include anything else but the raw value.
+
+ Make sure your program compiles successfully with no errors or warnings.
+
+
+ 
---
+++ main.c
@@ -0,0 +1,36 @@
+/* main.c - <one-line-description>
+   Copyright (c) <year>, <author(s)-name>  <email(s)>
+   This file is part of <project>
+   <project> is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+   
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+   
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include <stdio.h>
+
+float average(float a, float b, float c)
+{
+  return 0;
+}
+
+int main()
+{
+  float v1, v2, v3, result;
+
+  v1 = 10;
+  v2 = 20;
+  v3 = 60; 
+  
+  result = average (v1, v2, v3);
+
+  return 0 ;
+}
